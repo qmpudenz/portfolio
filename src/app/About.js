a8dedc1dfc848a33f5271fd073054d3f887cb2e9
Quinn <quinnpudenz@gmail.com>
2023-09-04 15:06:52 -0500

    mobile testing

--- src/app/About.js
+++ src/app/About.js
@@ -13,8 +13,8 @@ export default function About() {
       <section id="about" className="flex flex-col items-center">
         <div className="py-auto align-center container mx-10 flex w-auto flex-col items-center justify-center rounded-t-[45px] bg-neutral-900 px-10 pb-20  max-md:mx-5 max-md:h-[77vh] md:h-[88vh] md:flex-row lg:h-[88vh]">
           <div className=" flex flex-col items-center text-left md:w-1/2 md:items-start lg:flex-grow">
-            <div className="align-center flex">
-              <h1 className="title-font text-left font-medium text-white max-md:text-4xl sm:w-1/2 sm:text-5xl md:w-full md:text-5xl lg:text-5xl xl:text-7xl">
+            <div className="align-center flex flex-col sm:flex-row">
+              <h1 className="title-font text-left text-lg font-medium text-white sm:w-1/2 sm:text-3xl md:w-full md:text-5xl lg:text-5xl xl:text-7xl">
                 <br className="flex font-medium" />
                 Let&apos;s bring your ideas to life.
               </h1>
@@ -30,25 +30,25 @@ export default function About() {
                 </div>
               </div>
             </div>
-            <p className="sm:text-m mb-4 mt-8 leading-relaxed text-white md:text-lg lg:text-xl xl:text-2xl">
+            <p className="sm:text-md mb-4 mt-8 text-sm leading-relaxed text-white max-sm:text-sm md:text-lg lg:text-xl xl:text-2xl">
               I&apos;m a freelance developer based out of Kansas City,
               passionate about building beautiful, functional websites and
               applications.
             </p>
-            <p className="md:text-md mb-8 mt-4 font-extralight leading-relaxed text-white lg:text-lg xl:text-xl">
+            <p className="md:text-md mb-8 mt-4 font-extralight leading-relaxed text-white max-sm:text-sm lg:text-lg xl:text-xl">
               I specialize in React, Next.js, and Node.js, but I&apos;m always
               learning new technologies and frameworks.
             </p>
             <div className="flex justify-center">
               <a
                 href="#contact"
-                className="inline-flex rounded border-0 bg-white px-6 py-2 text-lg text-black hover:bg-sky-400 focus:outline-none"
+                className=" inline-flex rounded border-0 bg-white px-6 py-2 text-[10px] text-black hover:bg-sky-400  focus:outline-none md:text-lg"
               >
                 Reach Out
               </a>
               <a
                 href="#projects"
-                className="ml-4 inline-flex rounded border-0 bg-black px-6 py-2 text-lg text-white hover:bg-neutral-700 focus:outline-none"
+                className="ml-4 inline-flex rounded border-0 bg-black px-6 py-2 text-[10px] text-white  hover:bg-neutral-700 focus:outline-none md:text-lg"
               >
                 See My Past Work
               </a>
