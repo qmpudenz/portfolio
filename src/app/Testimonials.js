2321291ac2668a44f71d67b5b2a1b5e065911b59
Quinn <quinnpudenz@gmail.com>
2023-09-20 17:07:39 -0500

    Testimonials slider added for mobile

--- src/app/Testimonials.js
+++ src/app/Testimonials.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { testimonials } from "./data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrown } from "@fortawesome/free-solid-svg-icons";
+import Siema from "siema";
 
 export default function Testimonials() {
+  useEffect(() => {
+    const slider = new Siema({
+      selector: ".siema-slider",
+      duration: 200,
+      easing: "ease-out",
+      perPage: 1, // Change this if you want more than one item per slide
+      startIndex: 0,
+      draggable: true,
+      threshold: 20,
+      loop: true, // This makes it loop around when it reaches the end
+    });
+
+    return () => {
+      slider.destroy();
+    };
+  }, []);
+
   return (
     <section id="testimonials" className="flex h-full justify-center">
       <div className="container mx-auto flex h-full flex-col justify-center px-5 text-center">
-        <div className="h-[55%]">
+        <div className="h-[15%]">
           <div className="flex flex-wrap justify-center">
             <UsersIcon className="inline-block w-10" />
             <h1 className="title-font pl-3 text-3xl font-medium text-white sm:text-4xl">
@@ -28,33 +46,35 @@ export default function Testimonials() {
         </div>
 
         <div className="mt-2 flex h-[85%] flex-wrap">
-          {testimonials.map((testimonial) => (
-            <div key={testimonial.id} className="w-full md:w-1/2">
-              <div className="mb-4 flex h-[95%] flex-col justify-between rounded bg-neutral-900 p-4">
-                <div>
-                  <TerminalIcon className="block w-8 text-gray-500" />
-                </div>
-                <div>
-                  <p className="h-[15vh] overflow-scroll text-lg font-light leading-relaxed">
-                    {testimonial.quote}
-                  </p>
-                </div>
+          <div className="siema-slider">
+            {testimonials.map((testimonial) => (
+              <div key={testimonial.id} className="w-full md:w-1/2">
+                <div className="mb-4 flex h-[95%] flex-col justify-between rounded bg-neutral-900 p-4">
+                  <div>
+                    <TerminalIcon className="block w-8 text-gray-500" />
+                  </div>
+                  <div>
+                    <p className="h-[55vh] overflow-scroll text-lg font-light leading-relaxed">
+                      {testimonial.quote}
+                    </p>
+                  </div>
 
-                <div>
-                  <div className="inline-flex items-center">
-                    <span className="flex flex-grow flex-col pl-4">
-                      <span className="title-font text-2xl font-medium text-white">
-                        {testimonial.name}
+                  <div>
+                    <div className="inline-flex items-center">
+                      <span className="flex flex-grow flex-col pl-4">
+                        <span className="title-font text-2xl font-medium text-white">
+                          {testimonial.name}
+                        </span>
+                        <span className="text-xl font-extralight uppercase text-gray-500">
+                          {testimonial.company}
+                        </span>
                       </span>
-                      <span className="text-xl font-extralight uppercase text-gray-500">
-                        {testimonial.company}
-                      </span>
-                    </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
