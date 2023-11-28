37cef50092214ffb3626eb54518325b9263c4335
Quinn <quinnpudenz@gmail.com>
2023-11-28 09:28:25 -0600

    menu cleanup

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -3,11 +3,7 @@ import React, { useState } from "react";
 
 import { Inter_Tight, Agbalumo, Merriweather_Sans } from "next/font/google";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBars,
-  faTimes,
-  faChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const inter = Inter_Tight({
   display: "swap",
@@ -32,23 +28,23 @@ export default function Navbar() {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="static top-0 z-10  text-black md:sticky">
-      <div className="container mx-auto flex flex-row flex-wrap items-center justify-between px-[5%] md:flex-row">
-        {/* Name OR Links */}
-        <div className={`${merriweatherSans.className} flex-grow`}>
-          {isMobileMenuOpen ? (
-            <nav className="flex-wrap items-center justify-start py-1 text-black md:mr-auto md:border-l md:border-gray-700 md:py-1 md:pl-4">
-              <a href="#portfolio" className="mr-5 hover:text-white">
-                Portfolio
-              </a>
-              <a href="#skills" className="mr-5 hover:text-white">
-                Skills
-              </a>
-              <a href="#testimonials" className="mr-5 hover:text-white">
-                Testimonials
-              </a>
-            </nav>
-          ) : (
+    <header className="relative top-0 z-20 text-black md:sticky">
+      <div className="container mx-auto flex flex-wrap items-center justify-between px-[5%] md:flex-row">
+        {/* Toggle Between Logo/Name and Mobile Menu */}
+        {isMobileMenuOpen ? (
+          <nav className="flex flex-grow text-xl underline-offset-4">
+            <a href="#portfolio" className="block p-2 hover:underline">
+              Portfolio
+            </a>
+            <a href="#skills" className="block p-2 hover:underline">
+              Skills
+            </a>
+            <a href="#testimonials" className="block p-2 hover:underline">
+              Testimonials
+            </a>
+          </nav>
+        ) : (
+          <div className={`${merriweatherSans.className} flex-grow`}>
             <div
               className={`${agbalumo.className} title-font flex items-end text-2xl font-light text-black md:mb-0`}
             >
@@ -61,27 +57,35 @@ export default function Navbar() {
                 Freelance Developer
               </a>
             </div>
-          )}
-        </div>
+          </div>
+        )}
 
-        {/* Menu Icon: Hamburger OR Close(X) */}
-        <div className="flex justify-end">
-          <a
-            href="#"
-            className="color-white hover:text-white"
-            onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-          >
+        {/* Mobile Menu Icon */}
+        <div className="md:hidden">
+          <a href="#" onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}>
             <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
           </a>
         </div>
 
+        {/* Desktop Menu */}
+        <nav className="hidden text-xl underline-offset-4 md:flex md:items-center md:justify-end">
+          <a href="#portfolio" className="mr-5 hover:underline">
+            Portfolio
+          </a>
+          <a href="#skills" className="mr-5 hover:underline">
+            Skills
+          </a>
+          <a href="#testimonials" className="mr-5 hover:underline">
+            Testimonials
+          </a>
+        </nav>
+
         {/* Contact Button */}
         <a
           href="#contact"
-          className="raise mt-4 hidden items-center rounded border-0 bg-sky-400 px-3 py-1 text-base text-black hover:bg-sky-400 focus:outline-none md:mt-0"
+          className="raise mt-4 hidden items-center rounded bg-sky-400 px-3 py-1 text-base hover:bg-sky-600"
         >
-          Contact
-          <ArrowRightIcon className="ml-1 h-4 w-4" />
+          Contact <ArrowRightIcon className="ml-1 h-4 w-4" />
         </a>
       </div>
     </header>
